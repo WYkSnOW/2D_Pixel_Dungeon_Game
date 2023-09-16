7807da3b04765b8356a409ad140369ec84f33ef5
WYkSnOW <kizuner_tony@lawn-128-61-47-13.lawn.gatech.edu>
2023-09-16 17:09:22 -0400

    create playerView clss

---
+++ app/src/main/java/com/example/myapplication/PlayerView.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class PlayerView {
+}
