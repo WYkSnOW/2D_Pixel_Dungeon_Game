4695da66bac259a7d11cac51c3b371afcc7c5b04
WYkSnOW <kizuner_tony@lawn-128-61-47-13.lawn.gatech.edu>
2023-09-16 17:14:19 -0400

    create class of ConfigScreen

---
+++ app/src/main/java/com/example/myapplication/ConfigScreen.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+public class ConfigScreen {
+}
