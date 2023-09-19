efa054c24833e991dab6352c5d78b9d2a1df4e75
WYkSnOW <114266156+WYkSnOW@users.noreply.github.com>
2023-09-19 18:09:16 -0400

    update

--- app/src/main/java/com/example/myapplication/EndingScreen.java
+++ app/src/main/java/com/example/myapplication/EndingScreen.java
@@ -22,5 +22,13 @@ public class EndingScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button exit_btn = findViewById(R.id.goes_to_startingScreen);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
     }
 }
