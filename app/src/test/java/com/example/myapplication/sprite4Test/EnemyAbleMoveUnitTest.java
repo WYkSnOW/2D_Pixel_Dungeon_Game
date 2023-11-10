058501b550f249afbbb41ad412df53e156cc99ef
djain2022 <diya.jain248@gmail.com>
2023-11-10 11:56:46 -0500

    Added a unit test called Correct CompareDiffUnitTest.

---
+++ app/src/test/java/com/example/myapplication/sprite4Test/EnemyAbleMoveUnitTest.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.sprite4Test;
+import static org.junit.Assert.assertFalse;
+
+import com.example.myapplication.Model.environments.GameMap;
+
+import org.junit.Test;
+
+public class EnemyAbleMoveUnitTest {
+
+    @Test
+    public void enemyAbleMoveUnitTest() {
+        // here are the id of black that doesn't allow the monster to move
+        int [] notMoveAbleBlock = {59. 60, 150, 0};
+
+        for (int i : notMoveAbleBlock) {
+            assertFalse(GameMap.isMoveAbleBlock(i));
+        }
+
+    }
+}
