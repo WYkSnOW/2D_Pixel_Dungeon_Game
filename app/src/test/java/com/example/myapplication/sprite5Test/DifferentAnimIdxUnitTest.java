0bddb6fd9a0f94b6ef116e4be796a9dbf4da4e14
vguru23 <guru.vibha@gmail.com>
2023-11-27 22:38:49 -0500

    new unit test to check different animal animation

--- app/src/test/java/com/example/myapplication/sprite5Test/DifferentAnimIdxUnitTest.java
+++ app/src/test/java/com/example/myapplication/sprite5Test/DifferentAnimIdxUnitTest.java
@@ -6,12 +6,18 @@ import com.example.myapplication.Model.entities.enemies.enemyStartegy.EnemyStrat
 import com.example.myapplication.Model.entities.enemies.enemyStartegy.OgreStrategy;
 import com.example.myapplication.Model.entities.enemies.enemyStates.EnemyStates;
 
+import static org.junit.Assert.assertNotEquals;
+
+import com.example.myapplication.Model.entities.enemies.enemyStartegy.EnemyStrategy;
+import com.example.myapplication.Model.entities.enemies.enemyStartegy.OgreStrategy;
+import com.example.myapplication.Model.entities.enemies.enemyStates.EnemyStates;
+
 import org.junit.Test;
 
 public class DifferentAnimIdxUnitTest {
     @Test
     public void differentAnimIdx() {
-        //During different state, length of the animation is different.
+        //testing to see if animation length is different per enemy strategy
         EnemyStrategy enemyStrategy = new OgreStrategy();
         int maxIndex1 = enemyStrategy.getAnimMaxIndex(EnemyStates.ATK);
         int maxIndex2 = enemyStrategy.getAnimMaxIndex(EnemyStates.IDLE);
