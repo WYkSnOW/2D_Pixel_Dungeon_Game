c7821cdb5e7d0c4f27dea3760638884a544fd05a
NikhilSura04 <nikhil_surapaneni@outlook.com>
2023-11-28 01:51:24 -0500

    edited sprint 4 baseOnScore test case

--- app/src/test/java/com/example/myapplication/sprite4Test/LocalBaseOnScoreLenUnitTest.java
+++ app/src/test/java/com/example/myapplication/sprite4Test/LocalBaseOnScoreLenUnitTest.java
@@ -14,13 +14,20 @@ public class LocalBaseOnScoreLenUnitTest {
     public void localBaseOnScoreLen() {
         Score s1 = new Score(20, 1, "Nikhil", true);
         Score s2 = new Score(0, 1, "Nikhil2", true);
+        Score s3 = new Score(0, 1, "Nikhil3", true);
+        Score s4 = new Score(0, 1, "Nikhil4", true);
 
         int offsetOne =
                 Leaderboard.getInstance().calScoreOffset(Integer.toString(s1.getScore()).length());
         int offsetTwo =
                 Leaderboard.getInstance().calScoreOffset(Integer.toString(s2.getScore()).length());
+        int offsetThree =
+                Leaderboard.getInstance().calScoreOffset(Integer.toString(s3.getScore()).length());
+        int offsetFour =
+                Leaderboard.getInstance().calScoreOffset(Integer.toString(s4.getScore()).length());
 
         assertNotEquals(offsetOne, offsetTwo);
+        assertNotEquals(offsetThree, offsetFour);
     }
 }
 
